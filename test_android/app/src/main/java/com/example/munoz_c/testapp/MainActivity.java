97430ecf3809b04a26c586bb3693e9c9a843ca93
Carlos Munoz <munoz_c@subway.com>
2018-05-15 17:05:54 -0400

    push app

---
+++ test_android/app/src/main/java/com/example/munoz_c/testapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.munoz_c.testapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public int addThem(int v1,int v2){
+        return v1+v2;
+    }
+}
